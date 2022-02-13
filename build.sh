0a4a55564badcc1197166d8849488de59e4365e9
blacksky <blacksky3@tuta.io>
2022-02-13 18:10:59 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -33,7 +33,7 @@ cp -r gcc-git gcc-git-stage1
 
 cp -r libtool-git libtool-git-stage1
 
-cp -r lib32-libltd-git lib32-libltd-git-stage1
+cp -r lib32-libltdl-git lib32-libltd-git-stage1
 
 # create stage 2 build dir
 
@@ -47,7 +47,7 @@ cp -r gcc-git gcc-git-stage2
 
 cp -r libtool-git libtool-git-stage2
 
-cp -r lib32-libltd-git lib32-libltd-git-stage2
+cp -r lib32-libltdl-git lib32-libltd-git-stage2
 
 # build stage1
 
@@ -61,7 +61,7 @@ cd gcc-git-stage1 && makepkg -si --nocheck && cd ${source}
 
 cd libtool-git-stage1 && makepkg -si --nocheck && cd ${source}
 
-cd lib32-libltd-git-stage1 && makepkg -si --nocheck && cd ${source}
+cd lib32-libltdl-git-stage1 && makepkg -si --nocheck && cd ${source}
 
 # build stage2
 
@@ -75,7 +75,7 @@ cd gcc-git-stage2 && makepkg -si --nocheck && cd ${source}
 
 cd libtool-git-stage2 && makepkg -si --nocheck && cd ${source}
 
-cd lib32-libltd-git-stage2 && makepkg -si --nocheck && cd ${source}
+cd lib32-libltdl-git-stage2 && makepkg -si --nocheck && cd ${source}
 
 # make a copy of every pkg in package dir
 
@@ -91,7 +91,7 @@ cp -v gcc-git-stage2/*.pkg.tar.zst package/
 
 cp -v libtool-git-stage2/*.pkg.tar.zst package/
 
-cp -v lib32-libltd-git-stage2/*.pkg.tar.zst package/
+cp -v lib32-libltdl-git-stage2/*.pkg.tar.zst package/
 
 # clean build dir
 
